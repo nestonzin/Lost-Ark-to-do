04d5d46ee6eea1b577065f43583e43d3ae80321b
nestonzin <dinestoor@outlook.com>
2022-12-17 20:16:32 -0300

    feat: :sparkles: add Head and metatag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Flex, Image } from "@chakra-ui/react";
+import Head from "next/head";
 import { Tasks } from "../Components/Todo/Tasks";
 import { DefaultLayout } from "../_layouts/default";
 
@@ -6,6 +7,13 @@ export default function Home() {
   return (
     <DefaultLayout>
       <Flex justifyContent="center" alignItems="center">
+        <Head>
+          <title>Lost Ark to do list</title>
+          <meta
+            name="description"
+            content="Organize seu farm diário e semanal com sua lista de tarefas."
+          />
+        </Head>
         <Tasks />
         <Image src="gunner.png" alt=" " maxW="20rem" w="100%" h="auto" />
       </Flex>
