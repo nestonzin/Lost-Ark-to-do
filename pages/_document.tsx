6f3c4ea0cdb5c94409e93cd4054e078acddf2265
nestonzin <dinestoor@outlook.com>
2022-12-20 16:57:05 -0300

    feat: :sparkles: add seo metatags and favicon

---
+++ pages/_document.tsx
@@ -0,0 +1,63 @@
+import type { DocumentContext } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+const APP_NAME = "Lost Ark to do list";
+
+class MyDocument extends Document {
+  static getInitialProps(ctx: DocumentContext) {
+    return Document.getInitialProps(ctx);
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+          <meta
+            name="description"
+            content="Organize seu farm diário e semanal com sua lista de tarefas."
+          />
+          <meta name="application-name" content={APP_NAME} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#FFFFFF" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="theme-color" content="#ffffff" />
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
