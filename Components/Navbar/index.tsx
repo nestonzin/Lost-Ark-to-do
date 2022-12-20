df845c862faca22d23f5367075074fcebd45a905
nestonzin <dinestoor@outlook.com>
2022-12-20 16:20:17 -0300

    refactor: :recycle: refactor navbar

--- Components/Navbar/index.tsx
+++ Components/Navbar/index.tsx
@@ -1,16 +1,21 @@
-import { Button, Flex, HStack, Image, useColorMode } from "@chakra-ui/react";
+import { Button, Flex, Image, useColorMode } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 export const Navbar = () => {
   const { toggleColorMode, colorMode } = useColorMode();
   return (
     <Flex justifyContent="space-between" alignItems="center" p="1rem">
-      <Image src="LostArkLogo.png" maxW={"10rem"} w="100%" h="auto" />
-      <HStack>
-        <Button onClick={toggleColorMode}>
-          {colorMode === "dark" ? <MoonIcon /> : <SunIcon />}
-        </Button>
-      </HStack>
+      <Image
+        src="LostArkLogo.png"
+        maxW={"10rem"}
+        w="100%"
+        h="auto"
+        alt="Logo do jogo Lost Ark"
+        ml="3rem"
+      />
+      <Button onClick={toggleColorMode}>
+        {colorMode === "dark" ? <MoonIcon /> : <SunIcon />}
+      </Button>
     </Flex>
   );
 };
