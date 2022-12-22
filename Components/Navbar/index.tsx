e75c31c78c4d061dc4d66928e7fcd3c0e4da1ad7
nestonzin <dinestoor@outlook.com>
2022-12-22 18:26:53 -0300

    fix: :bug: add link de volta pra home na logo da navbar

--- Components/Navbar/index.tsx
+++ Components/Navbar/index.tsx
@@ -1,22 +1,22 @@
-import { Button, Flex, Image, useColorMode, Link, Box } from "@chakra-ui/react";
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Button, Flex, Image, useColorMode, Link } from "@chakra-ui/react";
+import { MoonIcon, SunIcon, ArrowLeftIcon } from "@chakra-ui/icons";
 
 export const Navbar = () => {
   const { toggleColorMode, colorMode } = useColorMode();
   return (
     <Flex justifyContent="space-between" alignItems="center" p="1rem">
-      <Image
-        src="LostArkLogo.png"
-        maxW={"10rem"}
-        w="100%"
-        h="auto"
-        alt="Logo do jogo Lost Ark"
-        ml="3rem"
-      />
-      <Flex gap="1rem" alignItems="center">
-        <Link href="/price">
-          Veja os preços dos principais itens
-        </Link>
+      <Link href="/">
+        <Image
+          src="LostArkLogo.png"
+          maxW={"10rem"}
+          w="100%"
+          h="auto"
+          alt="Logo do jogo Lost Ark"
+          ml="3rem"
+        />
+      </Link>
+      <Flex gap="1rem" alignItems="center" justifyContent="center">
+        <Link href="/price">Veja os preços dos principais itens</Link>
         <Button onClick={toggleColorMode}>
           {colorMode === "dark" ? <MoonIcon /> : <SunIcon />}
         </Button>
